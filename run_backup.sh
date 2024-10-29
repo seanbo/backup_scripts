11b7bd2265fb415d32624016c283495b0926af3f
Sean Ryle <seanbo@users.noreply.github.com>
2024-10-29 17:15:02 -0400

    Corrected path

--- run_backup.sh
+++ run_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_DIR=/home/obnaes/backup
+SCRIPT_DIR=/home/seanbo/backup
 SCRIPT_FILE=backup.sh
 
-sudo /home/obnaes/backup/backup.sh "$@"
+sudo /home/seanbo/backup/backup.sh "$@"
 #sudo "$SCRIPT_DIR/$SCRIPT_FILE"
